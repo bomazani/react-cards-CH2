28302fb10bc0b5727228a8d28c0aa797a82c9496
Bob Hunt <bob.hunt.email@gmail.com>
2018-08-02 10:19:26 -0400

    copied code from react-cards-cohort2. changed all instances of react-cards-cohort2 to reeact-cards-CH2.

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+/*
+ * CardGroup
+ * This component takes Card components as children and renders them.
+ * Example usage:
+ *
+ *      <CardGroup>
+ *          <Card description="Trial" icon="fa-thumbs-o-up" price="Free!" />
+ *      </CardGroup>
+ *
+ * The above would render a card group with a single child in it.
+ *
+ */
+const CardGroup = (props) => (
+    <div className="cardGroup">
+        {props.children}
+    </div>
+);
+
+const Card = (props) => {
+    return <div className="card cardGroup__card">
+        <div className="card__description cardGroup__cardDescription">
+            <div className={"icon fa " + props.icon + " card__descriptionIcon"} />
+            <div className="card__descriptionText">
+                {props.description}
+                {props.hint && <React.Fragment><br/>({props.hint})</React.Fragment>}
+            </div>
+        </div>
+        <div className="card__price">{props.price}</div>
+    </div>
+};
+
+const App = () => (
+    <CardGroup>
+        <Card description="Trial" icon="fa-thumbs-o-up" price="Free" />
+        <Card description="Basic Tier" icon="fa-trophy" hint="most popular" price="$10.00" />
+        <Card description="Advanced Tier" icon="fa-bolt" hint="only for enterprise-level professionals" price="$6,000.00" />
+    </CardGroup>
+);
+
+export default App;
